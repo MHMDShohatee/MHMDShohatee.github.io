8fbb8601092b315bad4fa90e458971b91eef570d
MHMDShohatee <m.shohatee8@gmail.com>
2025-06-25 02:13:22 -0400

    Initial commit

---
+++ js/simple.js
@@ -0,0 +1,13 @@
+function showSidebar()
+{
+    const sidebar = document.querySelector('.nav-slider')
+
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() 
+{
+    const sidebar = document.querySelector('.nav-slider')
+
+    sidebar.style.display = 'none'
+}
